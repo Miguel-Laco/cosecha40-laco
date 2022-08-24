0e895dbbc8466e228b37b79b8378d5ba049ed1de
Miguel-Laco <magulaco@gmail.com>
2022-08-24 19:06:45 -0300

    Menú e-commerce
    Termino el desafío de la clase 3

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import NavBar from './components/NavBar';
 function App() {
   return (
     <div className="App">
-     <NavBar></NavBar>
+    <NavBar></NavBar>
     </div>
   );
 }
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,29 +1,29 @@
 const NavBar = () => {
     return (
-        <div className="row header-nav">
-                    <div className="col-auto">
+        <div className="header-nav">
+                    <div>
                         <nav>
                             <ul className="header-nav-lista" id="menu">
                                 <li className="header-nav-link"><a href="#menu">Menu</a></li>
+                                <li className="header-nav-link"><a href="#contacto">Contacto</a></li>
                                 <li className="header-nav-link header-nav-link-close">
                                     <a href="#"> 
                                         <img className="header-img-close" src={require("../img/close-icon-negro.png")} alt="icono cierre"></img>
                                     </a>
                                 </li>
-                                <li className="header-nav-link"><a href="#contacto">Contacto</a></li>
                             </ul>
                         </nav>
                         <a href="#menu" className="header-menu">
-                            <img className="header-menu-logo" src={require("../img/menu-movil.jpg")} alt="Logo menu mobile"></img>
+                            <img className="header-menu-logo" src={require("../img/menu-movil.png")} alt="Logo menu mobile"></img>
                         </a>
                     </div>
-                    <div className="col-auto header-nav-order">
+                    <div className="header-nav-order">
                         <a href="./index.html"> 
                             <img className="header-logo" src={require("../img/Cosecha40-logo-01.png")} alt="Logo Cosecha40"></img>
                         </a>
                     </div>
-                    <div className="col-auto">
-                        <a type="button" data-bs-toggle="modal" data-bs-target="#modalCarrito"> 
+                    <div>
+                        <a type="button"> 
                             <img className="header-carrito" src={require("../img/carrito-compras.png")} alt="Carrito de Compra"></img>
                         </a>
                     </div>
