21ab383d88879027884177adbe608abdf36117a5
Miguel-Laco <magulaco@gmail.com>
2022-10-13 16:08:00 -0300

    100% Responsive
    Agrego la lógica para el menu hamburguesa

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,30 +1,35 @@
 import  CartWidget  from "../CartWidget/CartWidget.js";
 import { Link } from "react-router-dom";
 import { CartContext } from "../../context/CartContext";
-import {useContext } from "react";
+import {useContext, useState } from "react";
 
 
 
 const NavBar = (props) => {
     //traigo el cart, para mostrar condicionalmente el CartWidget
     const {cart} = useContext(CartContext) 
+    const [menu, setMenu] = useState(true);
+
+    const handleClick = () => {
+        setMenu(current => !current);
+    }
+    const menuID = menu ? `menu` : `menu2`;
+
     return (
         <div className="header-nav">
                     <div>
                         <nav>
-                            <ul className="header-nav-lista" id="menu">
+                            <ul className="header-nav-lista" id={menuID} >
                                 <Link className="header-nav-link" to={`/category/Malbec`}>Malbec</Link>
                                 <Link className="header-nav-link" to={`/category/Cabernet`}>Cabernet</Link>
                                 <Link className="header-nav-link" to={`/category/Rosado`}>Rosado</Link>
-                                <li className="header-nav-link header-nav-link-close">
-                                    <Link to={`/`}> 
-                                        {<i className="fa-solid fa-xmark carrito"></i>}
-                                    </Link>
+                                <li className="header-nav-link header-nav-link-close" onClick={handleClick}>
+                                    {<i className="fa-solid fa-xmark carrito"></i>}
                                 </li>
                             </ul>
                         </nav>
                         <div className="header-menu">
-                            <img className="header-menu-logo" src={`img/menuMovilB.png`} alt="Logo menu mobile"></img>
+                            <img className="header-menu-logo" src={`img/menuMovilB.png`} alt="Logo menu mobile" onClick={handleClick}></img>
                         </div>
                     </div>
                     <div className="header-nav-order">
